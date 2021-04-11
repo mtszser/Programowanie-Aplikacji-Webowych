2a67c7cc3dad12ec594a734f61835141337338fb
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2021-04-11 14:18:34 +0200

    Created App

---
+++ Projekt-lab3/src/app.ts
@@ -0,0 +1,28 @@
+export class App {
+    opwApiKey = '2143d0a80d3b223e32953639018a12c4';
+    constructor() {
+        this.getCityInfo('zakopane')
+    }
+    async getCityInfo(city: string) {
+        const weather = await this.getWeather('zakopane');
+        this.saveData(weather);
+    }
+    async getWeather(city: string): Promise<any> {
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
+        const weatherResponse = await fetch(openWeatherUrl);
+        const weatherData = await weatherResponse.json();
+        console.log(weatherData);
+        return weatherData;
+    }
+    saveData(data: any) {
+        localStorage.setItem('weatherData', JSON.stringify(data));
+    }
+    getData() {
+        const data = localStorage.getItem('weatherData');
+        if (data) {
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
+    }
+}
